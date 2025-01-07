67ab9be7359a1a195e795edb2d411766f213efbc
Rommel de Torres <detorresrc@gmail.com>
2025-01-07 10:26:18 +0800

    Add loading state to workspace switcher; display skeleton while fetching workspaces

--- src/components/custom/workspace-switcher.tsx
+++ src/components/custom/workspace-switcher.tsx
@@ -1,45 +1,62 @@
-"use client"
-import { RiAddCircleFill } from "react-icons/ri"
+"use client";
+import { RiAddCircleFill } from "react-icons/ri";
 import { useRouter } from "next/navigation";
 
-import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
-import { Select, SelectItem, SelectTrigger, SelectValue, SelectContent } from "../ui/select";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+} from "../ui/select";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useCreateWorkspaceModal } from "@/features/workspaces/hooks/use-create-workspace-modal";
+import { Skeleton } from "../ui/skeleton";
 
 export const WorkspaceSwitcher = () => {
   const router = useRouter();
   const workspaceId = useWorkspaceId();
   const { open } = useCreateWorkspaceModal();
 
-  const { data: workspaces } = useGetWorkspaces();
-  
+  const { data: workspaces, isPending } = useGetWorkspaces();
+
   const onSelect = (value: string) => {
     router.push(`/workspaces/${value}`);
-  }
+  };
 
   return (
     <div className='flex flex-col gap-y-2'>
       <div className='flex items-center justify-between'>
-        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
-        <RiAddCircleFill onClick={() => open()} className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"/>
+        <p className='text-xs uppercase text-neutral-500'>Workspaces</p>
+        <RiAddCircleFill
+          onClick={() => open()}
+          className='size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition'
+        />
       </div>
-      <Select onValueChange={onSelect} value={workspaceId}>
-        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
-          <SelectValue placeholder="No workspace selected"/>
-        </SelectTrigger>
-        <SelectContent>
-          {workspaces?.documents.map((workspace) => (
-            <SelectItem key={workspace.$id} value={workspace.$id}>
-              <div className="flex justify-start items-center gap-3 font-medium">
-                <WorkspaceAvatar name={workspace.name} image={workspace.imageUrl}/>
-                <span className="truncate">{workspace.name}</span>
-              </div>
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      {isPending ? (
+        <Skeleton className='w-full h-10'/>
+      ) : (
+        <Select onValueChange={onSelect} value={workspaceId}>
+          <SelectTrigger className='w-full bg-neutral-200 font-medium p-1'>
+            <SelectValue placeholder='No workspace selected' />
+          </SelectTrigger>
+          <SelectContent>
+            {workspaces?.documents.map((workspace) => (
+              <SelectItem key={workspace.$id} value={workspace.$id}>
+                <div className='flex justify-start items-center gap-3 font-medium'>
+                  <WorkspaceAvatar
+                    name={workspace.name}
+                    image={workspace.imageUrl}
+                  />
+                  <span className='truncate'>{workspace.name}</span>
+                </div>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      )}
     </div>
-  )
-}
+  );
+};
